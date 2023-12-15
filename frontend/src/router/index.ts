8ed862555a49dd66e36c769a188220c29b4db05b
sebacoma <sebastian.concha@alumnos.ucn.cl>
2023-12-15 20:23:11 -0300

    todo el inicio

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import HomePage from '../views/HomePage.vue';
+import Login from '../views/Login.vue'
+import Signup from '../views/Signup.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,7 +13,17 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: HomePage
-  }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/Signup',
+    name: 'Signup',
+    component: Signup,
+  },
 ]
 
 const router = createRouter({
